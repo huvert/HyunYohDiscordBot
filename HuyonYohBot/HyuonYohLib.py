dc651c4d2018230006b8b89cfa41287d8eeadeaf
huvert <66591482+huvert@users.noreply.github.com>
2023-01-22 11:02:45 +0100

    Initial Commit

---
+++ HuyonYohBot/HyuonYohLib.py
@@ -0,0 +1,148 @@
+import requests
+import json
+from random import randint
+
+
+def fetchRandomQuote():
+    '''Fetch an random inspirational quote from API! Doc: https://zenquotes.io/api'''
+    return requests.get('https://zenquotes.io/api/random').json()[0]["q"].upper()
+
+
+def fetchTodaysQuote():
+    '''Fetch an todays inspirational quote from API! Doc: https://zenquotes.io/api'''
+    return requests.get('https://zenquotes.io/api/today').json()[0]["q"].upper()
+    
+
+def getVocabulary():
+    '''Return dataobject from JSON file'''
+    file = open('HyuonYohVocabulary.json')
+    vocabulary = json.load(file)
+    file.close()
+    return vocabulary
+
+
+def doubleLetters(word):
+    '''Check for double letters (Dobbelkonsonant)'''
+    for i in range (len(word)-1):
+        if word[i] == word[i+1]:
+            return f"{word[:i]}{word[i+1:]}"
+    return word
+
+
+def rapeWordEnding(word):
+    '''Misspronounce words like HAPPENING --> HAPPENINGING'''
+    if word[-3:] == "ING":
+        return f"{word[:-3]}INING"
+    return word
+
+
+def hyunYohify(quote):
+    '''Returns a quote in Hyuon Yoh style'''
+    vocabulary = getVocabulary()
+    for i, word in enumerate(quote):
+        try:
+            print(f'Word: {word}')
+            sign = 0
+            if word[-1] in ['.', ',' , ';', ':', '!', '?']:
+                sign = word[-1]     #Store last index to sign
+                word = word[:-1]    #Remove last index
+
+            if word in vocabulary:
+                if len(vocabulary[word]) == 1:
+                    word = vocabulary[word][0]
+                else:
+                    r = randint(1, len(vocabulary[word])) # 1 .. end
+                    word = vocabulary[word][r-1]
+
+            elif word[-1] == 'S':
+                word = word[:-1] #Strip S away
+                sign = 'S'       #Store the S for later
+                if word in vocabulary:
+                    if len(vocabulary[word]) == 1:
+                        word = vocabulary[word][0]
+                    else:
+                        r = randint(1, len(vocabulary[word])) # 1 .. end
+                        word = vocabulary[word][r-1]
+
+            else: 
+                if randint(0,100) > 20:   #chance to misspell words like BECOMING --> BECOMINING
+                    word = rapeWordEnding(word)
+
+                if randint(0,100) > 20:   #chance to misspell doubel letters
+                    word = doubleLetters(word)
+
+                if randint(0,100) > 20:   #change to misspell "TH"
+                    if "TH" in word:
+                        if randint(0,100) > 50:
+                            word = word.replace("TH", "T") #WORT --> WORT
+                        else:
+                            word = word.replace("TH", "HT") #WORT --> WORHT
+                
+                if randint(0,100) > 20:   #Chance to misspell EA
+                    if "EA" in word:
+                        if randint(0,100) > 50:
+                            word = word.replace("EA", "E") #DREAMS --> DREMS
+                        else:
+                            word = word.replace("EA", "AE") #DREAMS --> DRAEMS
+
+                if randint(0,100) > 20:   #Chance to misspell OU 
+                    if "OU" in word:
+                        if randint(0,100) > 50:
+                            word = word.replace("OU", "O") #AROUND --> AROND
+                        else:
+                            word = word.replace("OU", "UO") #AROUND --> ARUOND
+
+                if randint(0,100) > 20:   #Chance to misspell CH
+                    if "CH" in word:
+                        if randint(0,100) > 50:
+                            word = word.replace("CH", "SH") #AROUND --> AROND
+
+
+            if sign != 0:
+                word = f"{word}{sign}" #Add sign back to word
+            
+            quote[i] = word     #Write word back to quote
+        except:
+            print("[HYUNYOIFY] error at line 106")
+    quote = ' '.join(quote)
+    return quote
+
+
+def getHyonYohQuote(type = "random"):
+    '''Gets a HyuonYohified quote'''
+    if type == "random":
+        quote = fetchRandomQuote()
+    elif type == "today":
+        quote = fetchTodaysQuote()
+    else:
+        quote = fetchTodaysQuote()
+    #quote = "HAPPENING HAPPENING BECOMING BECOMING BECOMING HAPPENING"
+    quote = quote.replace("'","") #Hyon Yoh doesnt use '
+    quote = list(quote.split(" "))
+    return hyunYohify(quote)
+
+
+def getHyonYohSource():
+    '''Fetch a random source from JSON file'''
+    file = open('HyuonYohSources.json')
+    source = json.load(file)["SOURCES"]
+    file.close()
+    r = randint(0,len(source)-1)
+    return source[r]
+
+
+def getHyuonYohMessage(type = "random"):
+    quote = getHyonYohQuote(type = "random")
+    source = getHyonYohSource()
+    return f"{quote}\n\n~{source}"
+
+
+if __name__ == "__main__":
+    #quote = "How you think is as important as whats you think.".upper()
+    quote = getHyonYohQuote()
+    #print(quote)
+    source = getHyonYohSource()
+    #print(source)
+    hyuonYohPost = f"{quote}\n\n~{source}"
+    print(hyuonYohPost)
+    
---
+++ HuyonYohBot/bot.py
@@ -0,0 +1,144 @@
+import os
+import discord
+import requests
+import youtube_dl
+from HyuonYohLib import getHyuonYohMessage
+from HyuonYohLib import hyunYohify
+from discord.ext import commands
+import asyncio
+from random import randint
+import datetime
+
+
+TOKEN = "MTAzNTY1OTE2MzAwNDU3NTc0NA.GzguSQ.5cul7VBJKT90NRnDJqf-eofnBCURT9c3S2o7tM"
+PREFIX = '_'
+MEME_CH_ID = 1014781327347822633
+
+# Silence useless bug reports messages
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+bot = commands.Bot(PREFIX, description='Hyun Yoh Bot')
+
+
+for filename in os.listdir('./cogs'):   # ./ --> Current folder
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+msg = ""
+
+
+
+
+
+# ------------------------------------
+# ----------    Commands    ----------
+@bot.event
+async def on_ready():
+    print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(bot))
+    
+    # INITIAL WAIT
+    now = datetime.datetime.now()
+    today = datetime.date.today()
+    then = datetime.datetime(today.year,today.month,today.day,12,00,00)
+    wait_time = abs(then-now).total_seconds()
+    await asyncio.sleep(wait_time)
+
+    # LOOP FOR DAILY MESSAGE
+    while True:
+        r = randint(0,100)
+        if r < 10:      #Chance for an insult
+            msg = requests.get('https://evilinsult.com/generate_insult.php?lang=en').text.upper()
+            msg = msg.replace("'","") #Hyon Yoh doesnt use '
+            msg = list(msg.split(" "))
+            msg = hyunYohify(msg)
+        else:           # Chance for todays inspirational quote
+            msg = getHyuonYohMessage(type="today")
+        channel = bot.get_channel(MEME_CH_ID)
+        print(msg)
+        await channel.send(msg)
+        await asyncio.sleep(60*60*24)
+
+
+@bot.command()
+async def inspirational(ctx):
+    '''Returns a random inspirational quote'''
+    msg = getHyuonYohMessage(type="random")
+    print(msg)
+    await ctx.send(msg)
+
+
+@bot.command()
+async def todaysquote(ctx):
+    '''Returns todays inspirational quote'''
+    msg = getHyuonYohMessage(type="today")
+    print(msg)
+    await ctx.send(msg)
+
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency * 1000)}ms')
+
+
+@bot.command()
+async def clear(ctx, amount=1):
+    """Clear 'x' number of messages. ex: _clear 3"""
+    await ctx.channel.purge(limit=(amount+1))
+
+
+@bot.command()
+async def insult(ctx):
+    """Random insult"""
+    res = requests.get('https://evilinsult.com/generate_insult.php?lang=en').text.upper()
+    res = res.replace("'","") #Hyon Yoh doesnt use '
+    res = list(res.split(" "))
+    insult = hyunYohify(res)
+    await ctx.send(insult)
+
+
+@bot.command()
+async def nobody(ctx):
+    """Returns AI-generated image of a person"""
+    res = requests.get('https://thispersondoesnotexist.com/image')
+    if res.status_code == 200:
+        with open('nobody.png', 'wb') as f:
+            f.write(res.content)
+        with open('nobody.png', 'rb') as f:
+            await ctx.send("HERE YUO HEV IS BUTIFULL PESHON\n", file=discord.File(f))
+    else:
+        await ctx.send("SOMETHING WONG (((((")
+
+
+@bot.command()
+async def pwbreach(ctx, myPasswordSHA1):
+    """Checks for pw-breaches. Input: Your pw in SHA-1 format"""
+    res = requests.get(f"https://api.pwnedpasswords.com/range/{myPasswordSHA1[:5]}")
+    myPW = myPasswordSHA1[5:]
+    mes = "No breaches found! :D"
+    print(res.status_code)
+    if res.status_code == 200:
+        content = res.text.split("\r\n")
+        print(content)
+        for cnt in content:
+            if myPW == cnt[:35]:
+                print("[bot] Password has been breached!")
+                x = cnt.split(":")
+                mes = f"Your password has been found in breached files {x[1]} times!"
+    else:
+        mes = "SOMETHING WONG ((((("
+    await ctx.send(mes)
+
+
+@bot.command()
+async def HELPpwbreach(ctx):
+    """Shows more info about function 'pwbreach'"""
+    await ctx.send("""***DO NOT TYPE YOUR OWN UNENCRYPTED PASSWORD ON DISCORD!***\n
+    function should be used as follows:
+    \t \t_pwbreach 'Passowrd in format SHA-1'
+    ex:\t_pwbreach FFC0F2952FD99C4B9147D6A101D031DABB9\n
+    visit https://www.cleancss.com/sha1-hash-generator/ for SHA-1 convertion""")
+
+
+bot.run(TOKEN)
